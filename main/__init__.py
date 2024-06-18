908569312bda559488b1e539e94f4d2db921f7d0
Gagan <business@devgagan.in>
2024-06-18 20:54:49 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,24 +1,35 @@
-#Join me at telegram @dev_gagan
+#Join @dev_gagan
 
+from telethon.errors import FloodWaitError
+from datetime import datetime as dt, timedelta
+import asyncio
+import sys
 from pyrogram import Client
-
+import asyncio
+import uvloop
+from pyrogram.errors import FloodWait
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
-
 from decouple import config
-import logging, time, sys
+import logging, time
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("telethon").setLevel(logging.WARNING)
 
-# variables
+uvloop.install()
+
+ggn = "mongodb+srv://viktor55:Gagan@123@cluster0.4efvr6n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None)
+MONGODB_CONNECTION_STRING = config("MONGO", default=ggn)
+OWNER_ID = config("OWNER_ID", default=None)
+LOG_GROUP = config("LOG_GROUP", default=None)
 SUDO_USERS = []
 
 if len(AUTH) != 0:
@@ -26,8 +37,7 @@ if len(AUTH) != 0:
 else:
     SUDO_USERS = set()
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
-
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 userbot = Client("myacc",api_id=API_ID,api_hash=API_HASH,session_string=SESSION)
 
 try:
@@ -37,7 +47,7 @@ except BaseException:
     sys.exit(1)
 
 Bot = Client(
-    "SaveRestricted",
+    "ggnbot",
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
     api_hash=API_HASH
@@ -46,6 +56,4 @@ Bot = Client(
 try:
     Bot.start()
 except Exception as e:
-    # print(e)
-    # logger.info(e)
     sys.exit(1)
