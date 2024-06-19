7a76fd298001ef0d1142652232cb43596906f995
Gagan <business@devgagan.in>
2024-06-19 15:40:06 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -25,8 +25,8 @@ from pyrogram import Client, filters
 import subprocess
 
 MONGODB_CONNECTION_STRING = "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn"
-OWNER_ID = 123456 # edit this
-LOG_GROUP = -10012345 # edit this
+OWNER_ID = 7065117445 # edit this
+LOG_GROUP = -1001878947221 # edit this
 
 # MongoDB database name and collection name
 DB_NAME = "start_users"
