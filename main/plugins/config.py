a94f4553b3c4b56d6d59bfd34fb917c55aaf00ec
Gagan <business@devgagan.in>
2024-06-19 12:56:47 +0530

    Delete main/plugins/config.py

--- main/plugins/config.py
+++
@@ -1,3 +0,0 @@
-OWNER_ID = 1234567
-MONGODB_CONNECTION_STRING = "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn" #change it to yours
-LOG_GROUP = -10012345 # always with -100
