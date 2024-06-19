a1298ebac300f9b9f3430c6743109753df67962b
Gagan <business@devgagan.in>
2024-06-19 10:14:30 +0530

    Create config.py

---
+++ main/plugins/config.py
@@ -0,0 +1,3 @@
+OWNER_ID = 1234567
+MONGODB_CONNECTION_STRING = "mongodb+srv://viktor55:Gagan@123@cluster0.4efvr6n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #change it to yours
+LOG_GROUP = -10012345 # always with -100
