32c3de88fa7ceb8e65ed15068ab4cb77082a0163
Gagan <business@devgagan.in>
2024-06-29 11:21:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return """
-<center> 
-    <img src="https://devgagan.in/livebot.png" style="border-radius: 12px;"/> 
-</center> 
-<style>
-    body { 
-        background: antiquewhite;
-    }
-</style>"""
+    <center>
+        <img src="https://devgagan.in/livebot.png" style="border-radius: 12px;"/>
+    </center>
+    <style>
+        body {
+            background: antiquewhite;
+        }
+    </style>"""
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
