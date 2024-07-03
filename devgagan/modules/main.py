11becc0c49283edd6e7f50ec350eef982b16fd3d
Gagan <business@devgagan.in>
2024-07-03 13:26:42 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -147,16 +147,6 @@ buttons = InlineKeyboardMarkup(
     ]
 )
 
-
-@ggn.on_message(filters.command("start"))
-async def start(_,message):
-  join = await subscribe(_,message)
-  if join ==1:
-    return
-  await message.reply_photo(photo="https://graph.org/file/4e80dc2f4f6f2ddadb4d2.jpg",
-                                  caption=script.START_TXT.format(message.from_user.mention), reply_markup=buttons)
-
-
 @ggn.on_message(filters.regex(r'https?://[^\s]+'))
 async def single_link(_, message):
     user_id = message.chat.id
