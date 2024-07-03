517382a868af0d6935aacff2e399ebcebe03d2f1
Gagan <business@devgagan.in>
2024-07-03 12:35:02 +0530

    Update __init__.py

--- devgagan/__init__.py
+++ devgagan/__init__.py
@@ -24,20 +24,6 @@ app = Client(
     bot_token=BOT_TOKEN,
 )
 
-sexxx = Client(
-    ":seex:",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-)
-
-try:
-    sexxx.start()
-    print("Bot started ... ")
-except Exception:
-    print("Something went wrong")
-
-
 async def madarchod_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
     await app.start()
