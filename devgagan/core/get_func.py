4d77c41f1d11ed93baa60915d10cc69e5803f635
Gagan <business@devgagan.in>
2024-07-03 15:38:12 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -8,6 +8,7 @@ import subprocess
 import requests
 import re
 from devgagan import app
+from devgagan import batch
 from devgagan import sex as gf
 from pyrogram import filters
 from pyrogram.errors import ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid, PeerIdInvalid
@@ -292,6 +293,218 @@ async def copy_message_with_chat_id(client, sender, chat_id, message_id):
         await client.send_message(sender, error_message)
         await client.send_message(sender, f"Make Bot admin in your Channel - {target_chat_id} and restart the process after /cancel")
 
+
+async def s_msg(userbot, sender, edit_id, msg_link, i, message):
+    edit = ""
+    chat = ""
+    round_message = False
+    if "?single" in msg_link:
+        msg_link = msg_link.split("?single")[0]
+    msg_id = int(msg_link.split("/")[-1]) + int(i)
+
+    
+    if 't.me/c/' in msg_link or 't.me/b/' in msg_link:
+        if 't.me/b/' not in msg_link:
+            chat = int('-100' + str(msg_link.split("/")[-2]))
+        else:
+            chat = msg_link.split("/")[-2]       
+        file = ""
+        try:
+            chatx = message.chat.id
+            msg = await userbot.get_messages(chat, msg_id)
+            if msg.media:
+                if msg.media == MessageMediaType.WEB_PAGE:
+                    edit = await batch.edit_message_text(sender, edit_id, "Cloning...")
+                    await batch.send_message(sender, msg.text.markdown)
+                    await edit.delete()
+                    return
+            if not msg.media:
+                if msg.text:
+                    edit = await batch.edit_message_text(sender, edit_id, "Cloning...")
+                    devggn = await batch.send_message(sender, msg.text.markdown)
+                    await devggn.copy(LOG_GROUP)
+                    await edit.delete()
+                    return
+            
+            edit = await batch.edit_message_text(sender, edit_id, "Trying to Download...")
+            file = await userbot.download_media(
+                msg,
+                progress=progress_bar,
+                progress_args=("**__Downloading : __**\n",edit,time.time()))
+            
+            custom_rename_tag = get_user_rename_preference(chatx)
+            last_dot_index = str(file).rfind('.')
+            if last_dot_index != -1 and last_dot_index != 0:
+                ggn_ext = str(file)[last_dot_index + 1:]
+                if ggn_ext.isalpha() and len(ggn_ext) <= 4:
+                    original_file_name = str(file)[:last_dot_index]
+                    file_extension = str(file)[last_dot_index + 1:]
+                else:
+                    original_file_name = str(file)
+                    file_extension = 'mp4'
+            else:
+                original_file_name = str(file)
+                file_extension = 'mp4'
+
+            delete_words = load_delete_words(chatx)
+            for word in delete_words:
+                original_file_name = original_file_name.replace(word, "")
+            new_file_name = original_file_name + " " + custom_rename_tag + "." + file_extension
+            os.rename(file, new_file_name)
+            file = new_file_name            
+            
+            await edit.edit('`Preparing to Upload!`')
+
+            # c = await db.get_data(sender)
+            # caption = None
+            
+            # if c.get("caption"):
+            #     caption = c.get("caption")
+            # else:
+            #     caption = msg.caption
+            #     if c.get("clean_words"):
+            #         words = c.get("clean_words")
+            #         caption = remove_elements(words, caption)
+                    
+            #     if c.get("replace_txt") and c.get("to_replace"):
+            #         replace_txt = c.get("replace_txt")
+            #         to_replace = c.get("to_replace")
+            #         caption = replace_text(caption, replace_txt, to_replace)
+
+            
+            if msg.media == MessageMediaType.VIDEO and msg.video.mime_type in ["video/mp4", "video/x-matroska"]:
+
+                metadata = video_metadata(file)      
+                width= metadata['width']
+                height= metadata['height']
+                duration= metadata['duration']
+
+                if duration <= 300:
+                    devggn = await batch.send_video(chat_id=sender, video=file, caption=caption, height=height, width=width, duration=duration, thumb=None, progress=progress_bar, progress_args=('**UPLOADING:**\n', edit, time.time())) 
+                    await devggn.copy(LOG_GROUP)
+                    await edit.delete()
+                    return
+                
+                delete_words = load_delete_words(sender)
+                custom_caption = get_user_caption_preference(sender)
+                original_caption = msg.caption if msg.caption else ''
+                final_caption = f"{original_caption}" if custom_caption else f"{original_caption}"
+                lines = final_caption.split('\n')
+                processed_lines = []
+                for line in lines:
+                    for word in delete_words:
+                        line = line.replace(word, '')
+                    if line.strip():
+                        processed_lines.append(line.strip())
+                final_caption = '\n'.join(processed_lines)
+                replacements = load_replacement_words(sender)
+                for word, replace_word in replacements.items():
+                    final_caption = final_caption.replace(word, replace_word)
+                caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}"
+
+                target_chat_id = user_chat_ids.get(chatx, chatx)
+                
+                thumb_path = await screenshot(file, duration, chatx)              
+                try:
+                    devggn = await batch.send_video(
+                        chat_id=target_chat_id,
+                        video=file,
+                        caption=caption,
+                        supports_streaming=True,
+                        height=height,
+                        width=width,
+                        duration=duration,
+                        thumb=thumb_path,
+                        progress=progress_bar,
+                        progress_args=(
+                        '**UPLOADING:**\n',
+                        edit,
+                        time.time()
+                        )
+                       )
+                    await devggn.copy(LOG_GROUP)
+                except:
+                    await batch.edit_message_text(sender, edit_id, "The bot is not an admin in the specified chat.")
+
+                os.remove(file)
+                    
+            elif msg.media == MessageMediaType.PHOTO:
+                await edit.edit("**`Uploading photo...`")
+                delete_words = load_delete_words(sender)
+                custom_caption = get_user_caption_preference(sender)
+                original_caption = msg.caption if msg.caption else ''
+                final_caption = f"{original_caption}" if custom_caption else f"{original_caption}"
+                lines = final_caption.split('\n')
+                processed_lines = []
+                for line in lines:
+                    for word in delete_words:
+                        line = line.replace(word, '')
+                    if line.strip():
+                        processed_lines.append(line.strip())
+                final_caption = '\n'.join(processed_lines)
+                replacements = load_replacement_words(sender)
+                for word, replace_word in replacements.items():
+                    final_caption = final_caption.replace(word, replace_word)
+                caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}"
+
+                target_chat_id = user_chat_ids.get(sender, sender)
+                devggn = await batch.send_photo(chat_id=target_chat_id, photo=file, caption=caption)
+                await devggn.copy(LOG_GROUP)
+
+                thumb_path = thumbnail(chatx)
+                delete_words = load_delete_words(sender)
+                custom_caption = get_user_caption_preference(sender)
+                original_caption = msg.caption if msg.caption else ''
+                final_caption = f"{original_caption}" if custom_caption else f"{original_caption}"
+                lines = final_caption.split('\n')
+                processed_lines = []
+                for line in lines:
+                    for word in delete_words:
+                        line = line.replace(word, '')
+                    if line.strip():
+                        processed_lines.append(line.strip())
+                final_caption = '\n'.join(processed_lines)
+                replacements = load_replacement_words(chatx)
+                for word, replace_word in replacements.items():
+                    final_caption = final_caption.replace(word, replace_word)
+                caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}"
+
+                target_chat_id = user_chat_ids.get(chatx, chatx)
+                try:
+                    devggn = await batch.send_document(
+                        chat_id=target_chat_id,
+                        document=file,
+                        caption=caption,
+                        thumb=thumb_path,
+                        progress=progress_bar,
+                        progress_args=(
+                        '**`Uploading...`**\n',
+                        edit,
+                        time.time()
+                        )
+                    )
+                    await devggn.copy(LOG_GROUP)
+                except:
+                    await batch.edit_message_text(sender, edit_id, "The bot is not an admin in the specified chat.") 
+                
+                os.remove(file)
+                        
+            await edit.delete()
+        
+        except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
+            await batch.edit_message_text(sender, edit_id, "Have you joined the channel?")
+            return
+        except Exception as e:
+            await batch.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`\n\nError: {str(e)}')       
+        
+    else:
+        edit = await batch.edit_message_text(sender, edit_id, "Cloning...")
+        try:
+            chat = msg_link.split("/")[-2]
+            await copy_message_with_chat_id(batch, sender, chat, msg_id) 
+            await edit.delete()
+        except Exception as e:
+            await batch.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`\n\nError: {str(e)}')
 
 
 # ------------------------ Button Mode Editz FOR SETTINGS ----------------------------
