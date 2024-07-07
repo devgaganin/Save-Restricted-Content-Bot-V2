b8e64f537cc097482e099b4e6fcd24c2f69f369d
Gagan <business@devgagan.in>
2024-07-07 08:50:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "19748984"))
-API_HASH = getenv("API_HASH", "2141e30f96dfbd8c46fbb5ff4b197004")
-BOT_TOKEN = getenv("BOT_TOKEN", "7012175450:AAHEdufYSZUkRBYt7antQjjcGD7fW1SusqQ")
-OWNER_ID = int(getenv("OWNER_ID", "7065117445"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://forward:forward@cluster0.xowzpr4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = int(getenv("LOG_GROUP", "-1001878947221"))
-# SESSION = getenv("PYROGRAM_V2_SESSION", "")
-FORCESUB = getenv("FORCESUB", "save_restricted_content_bots")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
+LOG_GROUP = int(getenv("LOG_GROUP", ""))
+FORCESUB = getenv("FORCESUB", "")
