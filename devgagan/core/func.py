ffe8d601a6f978bca49a694252a14750b26276d6
Gagan <business@devgagan.in>
2024-07-07 14:59:12 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -110,7 +110,7 @@ async def progress_bar(current, total, ud_type, message, start):
 
         progress = "{0}{1}".format(
             ''.join(["🟢" for i in range(math.floor(percentage / 10))]),
-            ''.join(["🔴" for i in range(20 - math.floor(percentage / 10))]))
+            ''.join(["🔴" for i in range(10 - math.floor(percentage / 10))]))
             
         tmp = progress + PROGRESS_BAR.format( 
             round(percentage, 2),
