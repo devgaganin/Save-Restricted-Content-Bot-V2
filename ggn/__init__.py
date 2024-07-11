c958ca93b8e16cddfbbdaa944759b733a56326cb
Gagan <business@devgagan.in>
2024-07-11 07:37:08 +0530

    Update __init__.py

--- ggn/__init__.py
+++ ggn/__init__.py
@@ -1,28 +1,35 @@
-# Join @devggn
+
+#Join @devggn
 
 import sys
-import asyncio
-from pyrogram import Client, compose
+from pyrogram import Client
 from telethon.sync import TelegramClient
-# import uvloop
+import uvloop
 from config import API_ID, API_HASH, BOT_TOKEN
 
-device = "Telegram Android 10.11.1"
-# uvloop.install()
-
-# Telethon client setup
 bot = TelegramClient('premiumrepo', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-sex = TelegramClient('ssswewew', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
+Bot = Client(
+    "ggnbot",
+    bot_token=BOT_TOKEN,
+    api_id=int(API_ID),
+    api_hash=API_HASH,
+    workers=10,
+    sleep_threshold=20,
+    max_concurrent_transmissions=8
+    
+)    
+
+try:
+    Bot.start()
+except Exception as e:
+    sys.exit(1)
 
 modi = Client(
     "modibot",
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
-    api_hash=API_HASH,
-    device_model=device,
-    sleep_threshold=20,
-    max_concurrent_transmissions=10
+    api_hash=API_HASH
 )    
 
 try:
@@ -35,9 +42,9 @@ sigma = Client(
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
     api_hash=API_HASH,
-    device_model=device,
+    workers=10,
     sleep_threshold=20,
-    max_concurrent_transmissions=10
+    max_concurrent_transmissions=8
 )    
 
 try:
@@ -45,34 +52,4 @@ try:
 except Exception as e:
     sys.exit(1)
 
-# List of additional Pyrogram clients
-Bot = []
-
-# Create multiple additional Pyrogram clients
-for i in range(1, 4):
-    client_name = f"bot{i}"
-    client = Client(
-        client_name,
-        bot_token=BOT_TOKEN,
-        api_id=int(API_ID),
-        api_hash=API_HASH,
-        device_model=device,
-        workers=50,
-        sleep_threshold=20,
-        max_concurrent_transmissions=10
-    )
-    
-    # Append client to the list
-    clients.append(client)
-
-# Define async function to run the clients
-async def run_clients():
-    try:
-        await compose(clients)
-    except Exception as e:
-        sys.exit(1)
-
-# Run the async function in the main script
-def run_additional_clients():
-    asyncio.run(run_clients())
-    
+sex = TelegramClient('sexrepo', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
