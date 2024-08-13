8d9736df424d9a6ff77a042cf7b053b15ccf1082
githubsksgk <githubpersonaltestbot@gmail.com>
2024-08-13 11:29:08 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -89,7 +89,7 @@ PROGRESS_BAR = """\n
 **__Bytes__** : {0}%
 **__Speed__** : {3}/s
 **__Time__** : {4}\n\n
-**__Powered by [Team SPY](https://t.me/devggn)__**
+**__Powered by [Team SKS](https://https://t.me/advancedchats)__**
 """
 
 
