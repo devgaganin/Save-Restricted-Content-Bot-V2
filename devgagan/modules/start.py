2cb96112a575663faf2aab331afc3edad2a88d19
githubsksgk <githubpersonaltestbot@gmail.com>
2024-08-14 10:41:29 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -19,6 +19,6 @@ async def start(_, message):
     join = await subscribe(_, message)
     if join == 1:
         return
-    await message.reply_photo(photo="https://graph.org/Personal-Test-Bot-2-08-10",
+    await message.reply_photo(photo="https://graph.org/PERSONAL-BOT-2-08-14",
                               caption=script.START_TXT.format(message.from_user.mention), 
                               reply_markup=buttons)
