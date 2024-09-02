8f6da45f4ee109e2ef03781adbc99e1899fb35fd
xadarsh541 <150887157+xadarsh541@users.noreply.github.com>
2024-09-02 15:58:15 +0530

    Update batch.py

--- ggn/assets/batch.py
+++ ggn/assets/batch.py
@@ -161,7 +161,7 @@ async def run_batch(userbot, client, sender, countdown, link):
             await get_bulk_msg(userbot, client, sender, link, integer)
             protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
             await countdown.edit(count_down, 
-                                 buttons=[[Button.url("Join Channel", url="https://t.me/devggn")]])
+                                 buttons=[[Button.url("Join Channel", url="https://t.me/ilikeaadi")]])
             await asyncio.sleep(timer)
             await protection.delete()
         except IndexError as ie:
