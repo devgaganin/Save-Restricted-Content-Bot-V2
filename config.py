6c4bafba4227f6680627960e4cc927e1ddf7943f
xadarsh541 <150887157+xadarsh541@users.noreply.github.com>
2024-09-02 17:25:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "28316155"))
 API_HASH = getenv("API_HASH", "e3499ac331a8df93997c1680366f2327")
-BOT_TOKEN = getenv("BOT_TOKEN", "7258854679:AAEuBdaao0iLEwtPPATVxRapavb46_5blCY")
+BOT_TOKEN = getenv("BOT_TOKEN", "6388939305:AAE7H9ASuLt3PgFAP6FG4I_P9pD8RaKk3d8")
 OWNER_ID = int(getenv("OWNER_ID", "7473115116"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://dustofappearance678:g1ETBVtzGV9ZcPJK@cluster0.bkxjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002183779903"))
