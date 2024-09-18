fd2f7119a950db1eebf836945f88186d90326de7
Gagan <business@devgagan.in>
2024-09-18 11:04:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# devggn
+# devgaganin
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
 from os import getenv
@@ -10,4 +10,4 @@ OWNER_ID = int(getenv("OWNER_ID", ""))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
 LOG_GROUP = int(getenv("LOG_GROUP", ""))
 FORCESUB = getenv("FORCESUB", "")
-DEFAULT_SESSION = getenv("DEFAULT_SESSION", "")
+DEFAULT_SESSION = getenv("DEFAULT_SESSION", "") # this is jkust to help if you dont want to force your bot user to login or if they not interested
