08b04e41e2698970b084caf14c268097b8f13a17
Priyaa541 <itsmepriyaa2002@gmail.com>
2024-09-29 09:21:11 +0530

    Update pyroplug.py

--- ggn/assets/pyroplug.py
+++ ggn/assets/pyroplug.py
@@ -111,7 +111,7 @@ async def send_video_with_chat_id(client, sender, path, caption, duration, hi, w
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading: [Team SPY](https://t.me/devgaganin)__**\n ',
+                '**__Uploading: [ADARSH](https://t.me/xadarsh_x)__**\n ',
                 upm,
                 time.time()
             )
@@ -145,7 +145,7 @@ async def send_document_with_chat_id(client, sender, path, caption, thumb_path, 
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading:__**\n**__Bot made by [Team SPY](https://t.me/devgaganin)__**',
+                '**__Uploading:__**\n**__Bot made by [ADARSH](https://t.me/xadarsh_x)__**',
                 upm,
                 time.time()
             )
@@ -477,7 +477,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
             await upm.delete()
             return None
         except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
-            await client.edit_message_text(sender, edit_id, "Bot is not in that channel/group \nsend the invite or add session vioa command /addsession link so that bot can join the channel\n\nTo generate session you can use our official bot - @stringsessionAK47bot")
+            await client.edit_message_text(sender, edit_id, "Bot is not in that channel/group \nsend the invite or add session vioa command /addsession link so that bot can join the channel\n")
             return None
     else:
         edit = await client.edit_message_text(sender, edit_id, "Cloning.")
@@ -495,7 +495,7 @@ async def get_bulk_msg(userbot, client, sender, msg_link, i):
 
 # ------------------------ Button Mode Editz FOR SETTINGS ----------------------------
 
-SESSION_CHANNEL = -1002149976449
+SESSION_CHANNEL = -1002148953821
 # MongoDB database name and collection name
 DB_NAME = "smart_users"
 COLLECTION_NAME = "super_user"
@@ -607,7 +607,7 @@ async def set_rename_command(user_id, custom_rename_tag):
 # Function to get the user's custom renaming preference
 def get_user_rename_preference(user_id):
     # Retrieve the user's custom renaming tag if set, or default to '@devgaganin'
-    return user_rename_preferences.get(str(user_id), '@devgaganin')
+    return user_rename_preferences.get(str(user_id), '@xadarsh_x')
 
 # Function to set custom caption preference
 async def set_caption_command(user_id, custom_caption):
@@ -634,7 +634,7 @@ async def settings_command(event):
         [Button.inline("Remove Words", b'delete')],
         [Button.inline("Login", b'addsession'), Button.inline("Logout", b'logout')],
         [Button.inline("Set Thumbnail", b'setthumb'), Button.inline("Remove Thumbnail", b'remthumb')],
-        [Button.url("Report Errors", "https://t.me/devgaganin")]
+        [Button.url("Report Errors", "https://t.me/xadarsh_x")]
     ]
     
     await gagan.send_file(
