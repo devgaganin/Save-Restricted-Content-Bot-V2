2121e8398a656a247f1797fac6376d6c3c4cd3bf
yourdilip <108778505+yourdilip@users.noreply.github.com>
2024-10-30 16:05:05 +0530

    ▰▱

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -108,8 +108,8 @@ async def progress_bar(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["🟢" for i in range(math.floor(percentage / 10))]),
-            ''.join(["🔴" for i in range(10 - math.floor(percentage / 10))]))
+            ''.join(["▰" for i in range(math.floor(percentage / 10))]),
+            ''.join(["▱" for i in range(10 - math.floor(percentage / 10))]))
             
         tmp = progress + PROGRESS_BAR.format( 
             round(percentage, 2),
