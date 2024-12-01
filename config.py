007dac467bd55d78dad1a51882522c303056ea87
krishna931074 <kumarrohit92572@gmail.com>
2024-12-02 01:47:21 +0530

    krish Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+API_ID = int(getenv("API_ID", "21567814"))
+API_HASH = getenv("API_HASH", "cd7dc5431d449fd795683c550d7bfb7e")
+BOT_TOKEN = getenv("BOT_TOKEN", "7649708076:AAE-4tGnslvR8cmk7J5OuRuRlEHqsZpzWYc")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "21567814").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://krishna931074:4L2TsfrwBSOu3sxB@cluster0.2p4co.mongodb.net/")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
