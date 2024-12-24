b43a013318b855a7c0116a01d455be71f6e5bb12
Gagan <business@devgagan.in>
2024-12-24 10:39:26 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -83,12 +83,12 @@ async def get_seconds(time_string):
 
 
 
-
 PROGRESS_BAR = """\n
-**__Completed__** : {1}/{2}
-**__Bytes__** : {0}%
-**__Speed__** : {3}/s
-**__Time__** : {4}
+│ **__Completed:__** {1}/{2}
+│ **__Bytes:__** {0}%
+│ **__Speed:__** {3}/s
+│ **__ETA:__** {4}
+╰─────────────────────╯
 """
 
 
@@ -108,8 +108,8 @@ async def progress_bar(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["🟢" for i in range(math.floor(percentage / 10))]),
-            ''.join(["🔴" for i in range(10 - math.floor(percentage / 10))]))
+            ''.join(["♦" for i in range(math.floor(percentage / 10))]),
+            ''.join(["◇" for i in range(10 - math.floor(percentage / 10))]))
             
         tmp = progress + PROGRESS_BAR.format( 
             round(percentage, 2),
@@ -121,7 +121,7 @@ async def progress_bar(current, total, ud_type, message, start):
         )
         try:
             await message.edit(
-                text="{}\n\n{}".format(ud_type, tmp),)             
+                text="{}\n│ {}".format(ud_type, tmp),)             
                 
         except:
             pass
