b5da9811390ce95980c63e95fce37f6236c7533a
Gagan <business@devgagan.in>
2025-01-12 17:17:50 +0530

    Update __init__.py

--- devgagan/__init__.py
+++ devgagan/__init__.py
@@ -1,4 +1,17 @@
-#devggn
+# ---------------------------------------------------
+# File Name: __init__.py
+# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
+#              and uploading them back to Telegram.
+# Author: Gagan
+# GitHub: https://github.com/devgaganin/
+# Telegram: https://t.me/team_spy_pro
+# YouTube: https://youtube.com/@dev_gagan
+# Created: 2025-01-11
+# Last Modified: 2025-01-11
+# Version: 2.0.5
+# License: MIT License
+# ---------------------------------------------------
+
 import asyncio
 import logging
 from pyrogram import Client
