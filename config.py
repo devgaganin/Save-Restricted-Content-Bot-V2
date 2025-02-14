df3a215b4f87b638fa353d0cc8d8c25a572fd3c6
Gagan <business@devgagan.in>
2025-02-14 17:30:22 +0530

    ✅ Added old method of default session and invite link method

--- config.py
+++ config.py
@@ -26,4 +26,5 @@ WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
+DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
 INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
