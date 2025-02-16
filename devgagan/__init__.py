89036446a5e54516b1ba0e94365179cc1b2217e3
Gagan <business@devgagan.in>
2025-02-16 09:00:07 +0530

    Update __init__.py

--- devgagan/__init__.py
+++ devgagan/__init__.py
@@ -14,14 +14,15 @@
 
 import asyncio
 import logging
+import time
 from pyrogram import Client
 from pyrogram.enums import ParseMode 
 from config import API_ID, API_HASH, BOT_TOKEN, STRING, MONGO_DB, DEFAULT_SESSION
 from telethon.sync import TelegramClient
 from motor.motor_asyncio import AsyncIOMotorClient
-import time
 
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
@@ -39,11 +40,19 @@ app = Client(
     parse_mode=ParseMode.MARKDOWN
 )
 
-pro = Client("ggbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 
-sex = TelegramClient('sexrepo', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+if STRING:
+    pro = Client("ggbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
+else:
+    pro = None
 
-userrbot = Client("userrbot", api_id=API_ID, api_hash=API_HASH, session_string=DEFAULT_SESSION)
+
+if DEFAULT_SESSION:
+    userrbot = Client("userrbot", api_id=API_ID, api_hash=API_HASH, session_string=DEFAULT_SESSION)
+else:
+    userrbot = None
+
+telethon_client = TelegramClient('telethon_session', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
 # MongoDB setup
 tclient = AsyncIOMotorClient(MONGO_DB)
@@ -59,8 +68,6 @@ async def setup_database():
     await create_ttl_index()
     print("MongoDB TTL index created.")
 
-# You can call this in your main bot file before starting the bot
-
 async def restrict_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
     await setup_database()
@@ -68,13 +75,11 @@ async def restrict_bot():
     getme = await app.get_me()
     BOT_ID = getme.id
     BOT_USERNAME = getme.username
-    if getme.last_name:
-        BOT_NAME = getme.first_name + " " + getme.last_name
-    else:
-        BOT_NAME = getme.first_name
-    if STRING:
+    BOT_NAME = f"{getme.first_name} {getme.last_name}" if getme.last_name else getme.first_name
+    
+    if pro:
         await pro.start()
-    if DEFAULT_SESSION:
+    if userrbot:
         await userrbot.start()
 
 loop.run_until_complete(restrict_bot())
