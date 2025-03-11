8a761d6bf48218cbdcca307c152cb0c32b2ef914
Gagan <business@devgagan.in>
2025-03-11 22:17:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def welcome():
 
 if __name__ == "__main__":
     # Default to port 5000 if PORT is not set in the environment
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
